eab71b5139ddcab45ed9c0899f376ced37416265
Rodrigo Alba <rodrigoqalba@gmail.com>
2021-10-12 17:27:49 -0400

    commit tree 3c2f16054ad730d32a920e631f3e0f74eade33fc

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+
+let multiply = num1 * num2;
+
+let random = Math.ceil(Math.random() * 10);
+
+let num3 = 16;
+let num4 = 4;
+let mod = num4 % num3;
+
+let max = Math.max(1, 2, 3, 4, 20);
